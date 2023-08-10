ff76e3c9bc976024ae3f20c4ec90cd28d150b16f
erobles <erobles@intergrupo.com>
2023-08-10 18:52:18 -0500

    IMplementación localStorage

--- src/App.js
+++ src/App.js
@@ -8,24 +8,36 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import React from 'react';
 
 
-const defaultTodos = [
-  { text: 'Terminar curso de Drupal', completed: true },
-  { text: 'Terminar curso de React', completed: false },
-  { text: 'Buscar mentoría para desarrollo móvil', completed: false },
-  { text: 'Inscribirme a diplomado fullstack', completed: false },
-  { text: 'Trabajar con reactstates', completed: true },
-];
+// const defaultTodos = [
+//   { text: 'Terminar curso de Drupal', completed: true },
+//   { text: 'Terminar curso de React', completed: false },
+//   { text: 'Buscar mentoría para desarrollo móvil', completed: false },
+//   { text: 'Inscribirme a diplomado fullstack', completed: false },
+//   { text: 'Trabajar con reactstates', completed: true },
+// ];
 
 function App() {
 
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    parsedTodos = [];
+    localStorage.setItem('TODOS_V1', JSON.stringify(parsedTodos));
+
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
   //agrego estados
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSerachValue] = React.useState('');
   //console.log(`Los usuarios buscan ${searchValue}`);
 
 
   // contabilizo los totales de las tareas y las completadas
-  const totalTodos = defaultTodos.length;
+  const totalTodos = parsedTodos.length;
   const completedTodos = todos.filter(todo => !!todo.completed).length;
 
   //Estados deribados para la búsqueda
@@ -33,36 +45,49 @@ function App() {
 
   //console.log(searchedTodos);
 
+
+  //función para guardar TODOs
+  const saveTodos = (newTodos) => {
+
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
+
+
+  //función de tdos completados
   const completeTodo = (text) => {
 
     const newTodos = [...todos];
-
     const todoIndex = newTodos.findIndex(
       (todo) => todo.text == text
     );
-
-
     newTodos[todoIndex].completed = true;
-
-
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   //función para eliminar las tareas
+  // const deleteTodo = (text) => {
+
+  //   const newTodos = [...todos];
+
+  //   const todoIndex = newTodos.findIndex(
+  //     (todo) => todo.text == text
+  //   );
+  //   newTodos.splice(todoIndex, 1);
+
+  //   console.log(newTodos);
+  //   setTodos(newTodos);
+  // };
+
+
   const deleteTodo = (text) => {
-
     const newTodos = [...todos];
-
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-
-
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
     newTodos.splice(todoIndex, 1);
-
-    console.log(newTodos);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
+
 
 
 
